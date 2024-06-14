cd2c52443f51f2549bccd8ae451f5e8f433b5b8d
MarZ2David <163112202+MarZ2David@users.noreply.github.com>
2024-06-14 04:42:09 -0600

    Cuestionario de proyecto

---
+++ baseDePreguntas.js
@@ -0,0 +1,45 @@
+baseDePreguntas = [
+    {
+      pregunta: "¿Qué tipo de lenguaje se muestra?",
+      imagen: `https://i.ibb.co/SQSwV7v/js1.png`,
+      respuesta: `JavaScript`,
+      distractores: [`HTML`, `PHP`, `CSS`],
+      ayuda: `otro ejemplo te puede ayudar`,
+      ayudaImg: `https://i.ibb.co/QrtN4Jr/js2.png`,
+    },
+    {
+      pregunta: `¿La biblioteca JQuery es una de las más utilizadas para el lenguaje JavaScript?`,
+      respuesta: `Si`,
+      distractores: [`No`, `Tal vez`, `Ni tanto`],
+      ayuda: `la siguiente imagen puede ayudarte`,
+      ayudaImg: `https://i.ibb.co/9hLxqyr/js3.png`,
+    },
+    {
+      pregunta: `a que se le conoce como: conjunto de herramientas y librerías que se utilizan para desarrollar aplicaciones más fácilmente y de manera más eficiente`,
+      respuesta: `Framework`,
+      distractores: [`lenguaje`, `Responsive`, `biblioteca`],
+    },
+    {
+        pregunta: `es un lenguaje de estilos utilizado en el ámbito del diseño web para dotar un sitio web de detalles estéticos, interacción y aspecto visual.`,
+        respuesta: `CSS`,
+        distractores: [`HTML`, `JavaScript`, `C++`],
+    },
+    {
+    pregunta: `A que se le conoce como un programa o fragmento de código que está incluido dentro de un documento HTML.`,
+    respuesta: `Script`,
+    distractores: [`Framework`, `Código`, `item`],
+    },
+    {
+        pregunta: "¿Qué es lo que se muestra en la imagen?",
+        imagen: `https://i.ibb.co/mBRy9GM/js4.png`,
+        respuesta: `Estructura HTML5`,
+        distractores: [`HTML`, `Pagina Web`, `Código CSS`],
+        ayuda: `otro ejemplo te puede ayudar`,
+        ayudaImg: `https://i.ibb.co/brjskBJ/estructura-etiquetas-html.png`,
+    },
+    {
+        pregunta: `es un formato de texto sencillo para el intercambio de datos. Se trata de un subconjunto de la notación literal de objetos de JavaScript,`,
+        respuesta: `Json`,
+        distractores: [`Framework`, `Código`, `JavaScript`],
+    }
+  ];
